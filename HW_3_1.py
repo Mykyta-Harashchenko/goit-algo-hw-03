4561a141cfaef2aef177e602b11c706fb644223f
Mykyta-Harashchenko <garasenkonikita538@gmail.com>
2024-03-08 19:04:32 +0000

    First try

--- HW_3_1.py
+++ HW_3_1.py
@@ -1,11 +1,14 @@
-import datetime
+from datetime import datetime
 
 
 def get_days_from_today(date):
-    date = datetime.datetime.strptime(date,'%Y-%m-%d')
-    now = datetime.datetime.today()
-    difference = now - date
-    return difference
+    try:
+        date = datetime.strptime(date,'%Y-%m-%d')
+        now = datetime.today()
+        difference = (now - date).days
+        return difference
+    except ValueError:
+        return 'Wrong entering format, enter it in format "YYYY-MM-DD"'
 
-checking = get_days_from_today('2024-10-9')
+checking = get_days_from_today('2024.10.09')
 print(checking)
