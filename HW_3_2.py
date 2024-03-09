6f1b8ab115c7ae2fdb02bd2842c13389fdcd4b1e
Mykyta-Harashchenko <garasenkonikita538@gmail.com>
2024-03-09 21:52:16 +0000

    Second try

--- HW_3_2.py
+++ HW_3_2.py
@@ -2,12 +2,12 @@ import random
 
 def get_numbers_ticket(min, max, quantity:int):
     
-    if (1 < min < max <= 1000 and 1 <= quantity <= (max - min + 1)):
+    if not (1 < min < max <= 1000 and 1 <= quantity <= (max - min + 1)):
         return []
     else:
         second_try = random.sample(range(min, max+1), k=quantity)
-        return sorted(second_try)
+        return second_try
     
 
-checking = get_numbers_ticket(1, 10, 9)
+checking = get_numbers_ticket(2, 10, 5)
 print('Ваші лотерейні числа:', checking)
---
+++ Misha дырявый хуесос.py
@@ -0,0 +1,4 @@
+import random
+
+dog_sex_misha = random.randint(1, 2)
+print(dog_sex_misha) 
